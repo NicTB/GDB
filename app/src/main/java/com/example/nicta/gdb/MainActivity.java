0848ed415b2771be7845f167b7cf40016d16996f
NicTB <nictasseb@hotmail.com>
2017-02-15 14:46:48 -0500

    gdb

--- app/src/main/java/com/example/nicta/gdb/MainActivity.java
+++ app/src/main/java/com/example/nicta/gdb/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.nicta.gdb;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnCollection;
+        Button btnCreation;
+
+        btnCollection = (Button)findViewById(R.id.btnCollectionCartes);
+        btnCreation = (Button)findViewById(R.id.btnCreationDeck);
+
+        btnCollection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,CollectionActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnCreation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,CreationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
