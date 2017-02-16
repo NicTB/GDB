49319950a5b4f8b63c10050ace3ac1d7d9e5fc5a
NicTB <nictasseb@hotmail.com>
2017-02-16 16:37:21 -0500

    gdb

---
+++ app/src/main/java/com/example/nicta/gdb/Carte.java
@@ -0,0 +1,17 @@
+package com.example.nicta.gdb;
+
+/**
+ * Created by Nicolas on 16/02/2017.
+ */
+
+public class Carte {
+
+    public String nom;
+    public String capacite;
+    public String image;
+
+    public Carte(String img){
+        image=img;
+    }
+
+}
--- app/src/main/java/com/example/nicta/gdb/CollectionActivity.java
+++ app/src/main/java/com/example/nicta/gdb/CollectionActivity.java
@@ -2,6 +2,8 @@ package com.example.nicta.gdb;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CollectionActivity extends AppCompatActivity {
 
@@ -9,5 +11,29 @@ public class CollectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collection);
+
+        Button btnFiltre = (Button) findViewById(R.id.btnFiltre);
+        Button btnChercher = (Button) findViewById(R.id.btnChercher);
+
+
+
+        /*
+        btnFiltre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Carte c1 = new Carte("adrenaline_rush");
+                AfficherCarteActivity cdd = new AfficherCarteActivity(CollectionActivity.this, c1);
+                cdd.show();
+            }
+        });
+
+        btnChercher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Carte c2 = new Carte("aeromancy");
+                AfficherCarteActivity cdd = new AfficherCarteActivity(CollectionActivity.this, c2);
+                cdd.show();
+            }
+        });*/
     }
 }
